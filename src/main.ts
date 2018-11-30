b475d08736159a3933d8beb7dca551b9d8763bfe
lubambi <lubambi420@163.com>
2018-11-30 19:07:36 +0800

    feat:引入element-ui & axios封装

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import * as ElementUI from 'element-ui';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-time-select
+      v-model="value1"
+      :picker-options="{
+        start: '08:30',
+        step: '00:15',
+        end: '18:30'
+      }"
+      placeholder="选择时间">
+    </el-time-select>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value1: ''
+      };
+    }
+  }
+</script>
